e9fcf277ea299afd3f313b8cc17bda411e92e412
Dmitriy Begishev <mintdragon@yandex.ru>
2021-11-15 22:05:19 +0400

    add IntegerFiels defait

--- blog/models.py
+++ blog/models.py
@@ -2,5 +2,5 @@ from django.db import models
 
 class Jokes(models.Model):
     joke_text = models.CharField(max_length = 500)
-    joke_rating = models.IntegerField()
+    joke_rating = models.IntegerField(default = 0)
     
