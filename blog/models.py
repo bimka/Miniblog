5f13bf92d99200119d909361d0aba7ab0b2ed928
Dmitriy Begishev <mintdragon@yandex.ru>
2021-11-27 19:53:29 +0400

    27.11.21

--- blog/models.py
+++ blog/models.py
@@ -3,4 +3,5 @@ from django.db import models
 class Jokes(models.Model):
     joke_text = models.CharField(max_length = 500)
     joke_rating = models.IntegerField(default = 0)
+    date_of_creation = models.DateTimeField(auto_now_add=True)
     
--- miniblog/urls.py
+++ miniblog/urls.py
@@ -16,14 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls import include
-from django.views.generic import RedirectView
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('blog/', include('blog.urls')),
-    path('', RedirectView.as_view(url='/blog/', permanent=True)),
+    path('', include('blog.urls')),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
