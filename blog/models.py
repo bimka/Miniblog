b6aedf7a827ea7e926ea3885946b17ba86830d2b
Dmitriy Begishev <mintdragon@yandex.ru>
2021-12-15 21:56:39 +0400

    add owner

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 import random
 
 from django.db import models
+from django.conf import settings
 
 
 class Jokes(models.Model):
@@ -9,6 +10,8 @@ class Jokes(models.Model):
         )
     joke_rating = models.IntegerField(default = random.randint(1, 10))
     date_of_creation = models.DateTimeField(auto_now_add=True)
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL,
+        on_delete = models.CASCADE)
 
     def get_id(self):
         return self.id
